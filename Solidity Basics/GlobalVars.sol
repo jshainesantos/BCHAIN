fa3205246d8276f0174ce8097ef89a190d9a3ff4
Shaine Santos <90233510+jshainesantos@users.noreply.github.com>
2024-02-07 23:55:17 +0800

    put insight

--- Solidity Basics/GlobalVars.sol
+++ Solidity Basics/GlobalVars.sol
@@ -15,3 +15,6 @@ contract MyContract is GlobalVariable {
         return globalValue;
     }
 }
+
+//
+This Solidity code defines a contract with a global variable globalValue initialized to 100, and another contract MyContract inheriting this variable, providing a function to retrieve its value.

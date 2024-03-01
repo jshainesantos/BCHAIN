e7c412454f16bbf1022251a5ec52a2cfdf108edd
Shaine Santos <90233510+jshainesantos@users.noreply.github.com>
2024-03-01 14:18:17 +0800

    Update midtermquiz1.sol

--- Midterm Quiz 1 Hands-on Lab/midtermquiz1.sol
+++ Midterm Quiz 1 Hands-on Lab/midtermquiz1.sol
@@ -3,57 +3,43 @@ pragma solidity ^0.8.3;
 
 contract MQuizContract {
     address public owner;
-    uint256 public age;
-    uint256 public hoursWorked;
-    uint256 public hourlyRate;
-    uint256 public totalSalary;
+    mapping(address => Employee) public employees;
+
+    struct Employee {
+        uint256 age;
+        uint256 hoursWorked;
+        uint256 hourlyRate;
+        uint256 totalSalary;
+    }
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Not the contract owner.");
         _;
     }
 
-    modifier forHourlyRate(uint256 rate) {
-        require(rate > 0, "Invalid. Must be greater than zero.");
-        _;
-    }
-
-    modifier forHoursWorked(uint256 Hours) {
-        require(Hours > 0, "Invalid. Must be greater than zero.");
-        _;
-    }
-
-
-    function setAge(uint256 _age) external onlyOwner {
-        age = _age;
-    }
-
-    function setHrsWrk(uint256 _hoursWorked)
-        external
-        onlyOwner
-        forHoursWorked(_hoursWorked)
-    {
-        hoursWorked = _hoursWorked;
-    }
-
-    function setRate(uint256 _hourlyRate)
-        external
-        onlyOwner
-        forHourlyRate(_hourlyRate)
-    {
-        hourlyRate = _hourlyRate;
-    }
-
-    function calculateTotalSal() public onlyOwner {
-        require(
-            hourlyRate > 0 && hoursWorked > 0,
-            "Invalid. Must be greater than zero."
-        );
-        totalSalary = hourlyRate * hoursWorked;
-    }
-
-
     constructor() {
         owner = msg.sender;
     }
+
+    function setAge(address _employeeAddress, uint256 _age) external onlyOwner {
+        employees[_employeeAddress].age = _age;
+    }
+
+    function setHoursWorked(address _employeeAddress, uint256 _hoursWorked) external onlyOwner {
+        employees[_employeeAddress].hoursWorked = _hoursWorked;
+    }
+
+    function setHourlyRate(address _employeeAddress, uint256 _hourlyRate) external onlyOwner {
+        employees[_employeeAddress].hourlyRate = _hourlyRate;
+    }
+
+    function calculateTotalSal(address _employeeAddress) external onlyOwner {
+        Employee storage employee = employees[_employeeAddress];
+        require(
+            employee.hourlyRate > 0 && employee.hoursWorked > 0,
+            "Invalid hourly rate or hours worked. Must be greater than zero."
+        );
+        employee.totalSalary = employee.hourlyRate * employee.hoursWorked;
+    }
 }
+
